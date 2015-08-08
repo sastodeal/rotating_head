00cd6a010dc6e9d8ce49f52a27e15fa572b25daa
Paul Miller <paul@paulmillr.com>
2015-08-08 11:39:46 +0545

    First Commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+#Rotating Heads
+
+This plugin was written for use in the [Anthropose](http://anthropose.com/product/aristle/) website in order to show how sunglasses would look like. This is actually a very low tech solution to a virtual try-on implemented in the Rayban website and is heavily inspired from the [Warby Parker](https://www.warbyparker.com/) website.
+
+##General Idea
+1. The general idea in this plugin is to use a [sprite image](https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/CSS_Image_Sprites) so that all angles are loaded simultaneously.
+2. If the number of images are 5 then the container is also divided into 5 parts.
+3. Hover on each part of the container will change the background position of the sprite to match accordingly.
+
+##Options
+###Options include:
+1. total_banner_width in pixels *(width of entire sprite )*
+2. total_width in pixels *(width of each individual item image in sprite)*
+3. Currently the number of sprite images has to be 5.
+
+##Usage
+> $("#rotate-container").rotateHeads();
+> $("#rotate-container").rotateHeads({
+    total_banner_width: 2551,
+    total_width: 431
+});
---
+++ src/rotating-heads.js
@@ -0,0 +1,55 @@
+/**
+* Author: Sodhan Manandhar
+* Description: Rotate heads on mouse hover
+* Limitation: Currently only supports 5 images
+*/
+(function ( $ ) {
+
+	$.fn.rotateHeads = function( options ){
+
+			 var settings = $.extend({
+					 total_banner_width: 2155,
+					 total_width: 431,
+					 number_of_images: 5
+			 }, options );
+
+		const single_width = settings.total_width / settings.number_of_images;
+
+		var rotate_container = this;
+
+	 		rotate_container.mousemove(function( e ){
+
+							var parentOffset = $(this).parent().offset();
+							var relativeXPosition = (e.pageX - parentOffset.left); //offset -> method allows you to retrieve the current position of an element 'relative' to the document
+
+							if( relativeXPosition > 0 && relativeXPosition <= single_width ){
+									$(this).css("background-position", settings.total_width * 0 +" 0");
+							}
+
+							if( relativeXPosition > single_width && relativeXPosition <= (single_width * 2) ){
+								$(this).css("background-position", "-" + settings.total_width * 1 +"px" + " 0");
+							}
+
+							if( relativeXPosition > (single_width * 2) && relativeXPosition <= (single_width * 3) ){
+								$(this).css("background-position", "-" + settings.total_width * 2 +"px" + " 0");
+							}
+
+							if( relativeXPosition > (single_width * 3) && relativeXPosition <= (single_width * 4) ){
+								$(this).css("background-position", "-" + settings.total_width * 3 +"px" + " 0");
+							}
+
+							if( relativeXPosition > (single_width * 4) && relativeXPosition <= (single_width * 5) ){
+								$(this).css("background-position", "-" + settings.total_width*4 +"px" + " 0");
+							}
+
+							if( relativeXPosition > (single_width * 5) && relativeXPosition <= (single_width * 6) ){
+								$(this).css("background-position", "-" + settings.total_width*5 +"px" + " 0");
+							}
+
+
+			});
+
+			return this;
+	};
+
+}( jQuery ));
